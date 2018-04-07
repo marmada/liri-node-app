fc71addec3a4c583f1bda7d7cd5996e2f28af217
Miriam Armada <miriam_armada@yahoo.com>
2018-04-07 16:43:01 -0700

    add imdb part

--- liri.js
+++ liri.js
@@ -1,17 +1,60 @@
-require("dotenv").config();
+require ('dotenv').config ();
 
-var request = require("request");
-var OMDb = keys.ombd;
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-// Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=" + , function(error, response, body) {
+var request = require ('request');
+var keys = require ('./keys.js');
+var fs = require ('fs');
 
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
+var OMDb = keys.omdb;
+var spotify = new Spotify (keys.spotify);
+var client = new Twitter (keys.twitter);
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+var input = process.argv;
+var inputA = process.argv[2];
+var inputB = '';
+
+for (var i = 3; i < input.length; i++) {
+  inputB =+ input[i] + ' ';
+}
+
+console.log (inputB);
+
+//function to search for movie using string with arguments from [3+]
+
+function imdbQuery () {
+  console.log (inputB);
+
+  var movie = ' ';
+
+  if (inputB === ' ') {
+    movie = 'Mr.+NoBody';
+  } else {
+    movie = inputB.split (' ').join ('+');
   }
-});
+
+  request (
+    'http://www.omdbapi.com/?t=' +
+      movie +
+      '&apikey=' +
+      OMDb +
+      '&plot=full&tomatoes=true',
+    function (error, response, body) {
+      // If the request is successful (i.e. if the response status code is 200)
+      if (!error && response.statusCode === 200) {
+        var data = JSON.parse (body);
+
+        console.log ('----------------------');
+        console.log ('Title: ' + data.title);
+        console.log ('Year Release: ' + data.year);
+        console.log ('IMDB Rating: ' + data.imdbRating);
+        console.log ('Rotten Tomatos Rating: ' + data.tomatoRating);
+        console.log ('Country: ' + data.Country);
+        console.log ('Language: ' + data.Language);
+        console.log ('Plot: ' + data.Plot);
+        console.log ('Actors: ' + data.Actors);
+        console.log ('----------------------');
+      }
+    }
+  );
+}
+
+//Request to OMDB
