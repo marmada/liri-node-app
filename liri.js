de8cac2ce93871507ad993e0955e96f0c500bb03
Miriam Armada <miriam_armada@yahoo.com>
2018-04-09 14:52:42 -0700

    Update

--- liri.js
+++ liri.js
@@ -50,20 +50,65 @@ function imdbQuery () {
     var data = JSON.parse (body);
 
     if (!error && response.statusCode === 200) {
-      console.log ('----------------------');
-      console.log ('Title: ' + data.Title);
-      console.log ('Year Release: ' + data.Year);
-      console.log ('IMDB Rating: ' + data.imdbRating);
-      console.log ('Rotten Tomatos Rating: ' + data.tomatoRating);
-      console.log ('Country: ' + data.Country);
-      console.log ('Language: ' + data.Language);
-      console.log ('Plot: ' + data.Plot);
-      console.log ('Actors: ' + data.Actors);
-      console.log ('----------------------');
+      console.log (
+        '----------------------\nTitle: ' +
+          data.Title +
+          '\nYear Release: ' +
+          data.Year +
+          '\nIMDB Rating: ' +
+          data.imdbRating +
+          '\nRotten Tomatos Rating: ' +
+          data.tomatoRating +
+          '\nCountry: ' +
+          data.Country +
+          '\nLanguage: ' +
+          data.Language +
+          '\nPlot: ' +
+          data.Plot +
+          '\nActors: ' +
+          data.Actors +
+          '\n----------------------'
+      );
+
+      fs.appendFile (
+        'log.txt',
+        '----------------------\r\nTitle: ' +
+          data.Title +
+          '\r\nYear Release: ' +
+          data.Year +
+          '\r\nIMDB Rating: ' +
+          data.imdbRating +
+          '\r\nRotten Tomatos Rating: ' +
+          data.tomatoRating +
+          '\r\nCountry: ' +
+          data.Country +
+          '\r\nLanguage: ' +
+          data.Language +
+          '\r\nPlot: ' +
+          data.Plot +
+          '\r\nActors: ' +
+          data.Actors +
+          '\r\n----------------------',
+        function (err) {
+          if (err) {
+            console.log (err);
+          }
+        }
+      );
     }
 
     if (data.Response == 'False') {
       console.log ('I did not find your movie, maybe try something else');
+
+      fs.appendFile (
+        'log.txt',
+        '\r\nI did not find your movie, maybe try something else',
+        function (err) {
+          if (err) {
+            console.log (err);
+          }
+        }
+      );
     } else {
       return 'N/A';
     }
@@ -82,109 +127,153 @@ function twitter () {
     if (!error) {
       //console.log(tweets);
       for (var i = 0; i < tweets.length; i++) {
-        console.log ('Date & Place:' + tweets[i].created_at);
-        console.log ('Tweet: ' + tweets[i].text);
+        console.log (
+          'Date & Place:' + tweets[i].created_at + '\nTweet: ' + tweets[i].text
+        );
+
+        fs.appendFile (
+          'log.txt',
+          '\r\nDate & Place:' +
+            tweets[i].created_at +
+            '\r\nTweet: ' +
+            tweets[i].text,
+          function (err) {
+            if (err) {
+              console.log (err);
+            }
+          }
+        );
       }
     }
   });
 }
 
+// searcg spotify
+
 function song () {
   var song = '';
 
   if (inputB == '') {
-    song = 'The Sign ace of base';
-
-    spotify.search ({type: 'track', query: song, limit: 1}, function (err, data) {
-      if (err) {
-        return console.log ('Error occurred: ' + err);
-      }
-  
-      console.log ('-----------------Spotify-this-song!--------------------\n');
-      //console.log(data.artists.items);
-      console.log ('Artists: ' + data.tracks.items[0].album.artists[0].name);
-      console.log ('Song Name: ' + data.tracks.items[0].name);
-      console.log ('Preview URL: ' + data.tracks.items[0].preview_url);
-      console.log ('External URL ' + data.tracks.items[0].external_urls.spotify);
-      console.log ('Album Name ' + data.tracks.items[0].album.name);
-      console.log (
-        '\n-----------------Brought to you a Liri Lola!--------------------'
-      );
-    });
-
+    song = 'The Sign Ace of base';
   } else {
-
     song = inputB;
-    spotify.search ({type: 'track', query: song, limit: 1}, function (err, data) {
-      if (err) {
-        return console.log ('Error occurred: ' + err);
-      }
-      
-      console.log ('\n-----------------Spotify-this-song!--------------------\n');
-      //console.log(data.tracks.items[0]);
-      console.log ('Artists: ' + data.tracks.items[0].album.artists[0].name);
-      console.log ('Song Name: ' + data.tracks.items[0].name);
-      console.log ('Preview URL: ' + data.tracks.items[0].preview_url);
-      console.log ('External URL ' + data.tracks.items[0].external_urls.spotify);
-      console.log ('Album Name ' + data.tracks.items[0].album.name);
-      console.log (
+  }
+
+  spotify.search ({type: 'track', query: song, limit: 1}, function (err, data) {
+    if (err) {
+      return console.log ('Error occurred: ' + err);
+    }
+
+    console.log (
+      '\n-----------------Spotify-this-song!--------------------\nArtists: ' +
+        data.tracks.items[0].album.artists[0].name +
+        '\nSong Name: ' +
+        data.tracks.items[0].name +
+        '\nPreview URL: ' +
+        data.tracks.items[0].preview_url +
+        '\nExternal URL ' +
+        data.tracks.items[0].external_urls.spotify +
+        '\nAlbum Name ' +
+        data.tracks.items[0].album.name +
         '\n-----------------Brought to you a Liri Lola!--------------------'
-      );
-    });
-  }
+    );
 
-  };
+    fs.appendFile (
+      'log.txt',
+      '\r\n-----------------Spotify-this-song!--------------------\r\nArtists: ' +
+        data.tracks.items[0].album.artists[0].name +
+        '\r\nSong Name: ' +
+        data.tracks.items[0].name +
+        '\r\nPreview URL: ' +
+        data.tracks.items[0].preview_url +
+        '\r\nExternal URL ' +
+        data.tracks.items[0].external_urls.spotify +
+        '\r\nAlbum Name ' +
+        data.tracks.items[0].album.name +
+        '\r\n-----------------Brought to you a Liri Lola!--------------------',
+      function (err) {
+        if (err) {
+          console.log (err);
+        }
+      }
+    );
+  });
+}
 
-  function justdoit(){
+// do what it says 
 
-    fs.readFile("random.txt", "utf8", function(error, data) {
+function justdoit () {
+  fs.readFile ('random.txt', 'utf8', function (error, data) {
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log (error);
+    }
+    // We will then print the contents of data
+    console.log (data);
 
-      // If the code experiences any errors it will log the error to the console.
-      if (error) {
-        return console.log(error);
-      }
-          // We will then print the contents of data
-      console.log(data);
-    
-      // Then split it by commas (to make it more readable)
-      var dataArr = data.split(",");
-      inputA = dataArr[0]
-      inputB = dataArr[1];
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split (',');
+    inputA = dataArr[0];
+    inputB = dataArr[1];
 
-      if (inputA == 'movie-this') {
-        imdbQuery (inputB);
-      }
-      
-      if (inputA == 'my-tweets') {
-        twitter (inputB);
-      }
-      
-      if (inputA == 'spotify-this-song') {
-        song (inputB);
-      }
-      
-      if(inputA == 'do-what-it-says'){
-      
-        justdoit(inputB);
-      }
-    });
-  }
+    if (inputA == 'movie-this') {
+      imdbQuery (inputB);
+    }
+
+    if (inputA == 'my-tweets') {
+      twitter (inputB);
+    }
+
+    if (inputA == 'spotify-this-song') {
+      song (inputB);
+    }
+
+    if (inputA == 'do-what-it-says') {
+      justdoit (inputB);
+    }
+  });
+}
 
 //Node Input
 
 if (inputA == 'movie-this') {
+  //log command
+
+  fs.appendFile ('log.txt', '\r\n' + inputA + ' ' + inputB, function (err) {
+    if (err) {
+      console.log (err);
+    }
+  });
+
   imdbQuery ();
 }
 
 if (inputA == 'my-tweets') {
+  fs.appendFile ('log.txt', '\r\n' + inputA + ' ' + inputB, function (err) {
+    if (err) {
+      console.log (err);
+    }
+  });
+
   twitter ();
 }
 
 if (inputA == 'spotify-this-song') {
+  fs.appendFile ('log.txt', '\r\n' + inputA + ' ' + inputB, function (err) {
+    if (err) {
+      console.log (err);
+    }
+  });
+
   song ();
 }
 
-if(inputA == 'do-what-it-says'){
+if (inputA == 'do-what-it-says') {
+  fs.appendFile ('log.txt', '\r\n' + inputA + ' ' + inputB, function (err) {
+    if (err) {
+      console.log (err);
+    }
+  });
 
-  justdoit();
+  justdoit ();
 }
